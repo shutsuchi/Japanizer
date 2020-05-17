8ec94fbf177c7773f15a3a847faa22213cbcd04e
shutsuchi <wwww.www.wshu@gmail.com>
2020-05-18 01:10:16 +0900

    [update] テストデータ具体化

--- db/seeds/post_comment.rb
+++ db/seeds/post_comment.rb
@@ -8,3 +8,81 @@ Post.all.each do |post|
     )
   end
 end
+
+# t.integer "user_id", null: false
+# t.integer "post_id", null: false
+# t.string "title", null: false
+# t.text "comment", null: false
+# t.float "rate", default: 0.0, null: false
+
+PostComment.create!(
+  user_id:    3,
+  post_id:    4,
+  title:      "綺麗",
+  comment:    "行ってみたい!",
+  rate:       5,
+)
+
+PostComment.create!(
+  user_id:    4,
+  post_id:    4,
+  title:      "Breathtaking...",
+  comment:    "I wanna go there someday...",
+  rate:       5,
+)
+
+PostComment.create!(
+  user_id:    6,
+  post_id:    4,
+  title:      "add this to my plan",
+  comment:    "Mt.Fuji...I decided to go next month!!",
+  rate:       5,
+)
+
+PostComment.create!(
+  user_id:    2,
+  post_id:    7,
+  title:      "いいな",
+  comment:    "自分で作ったうどんは美味しそうですね！",
+  rate:       4.5,
+)
+
+PostComment.create!(
+  user_id:    2,
+  post_id:    8,
+  title:      "いいな",
+  comment:    "みかんも美味しそう",
+  rate:       4.5,
+)
+
+PostComment.create!(
+  user_id:    3,
+  post_id:    11,
+  title:      "Great",
+  comment:    "You should go to MEIDO cafe!!",
+  rate:       4.5,
+)
+
+PostComment.create!(
+  user_id:    2,
+  post_id:    11,
+  title:      "Welcome",
+  comment:    "Welcome to Japan!",
+  rate:       4.5,
+)
+
+PostComment.create!(
+  user_id:    5,
+  post_id:    11,
+  title:      "good",
+  comment:    "this place was good for shopping.",
+  rate:       4.5,
+)
+
+PostComment.create!(
+  user_id:    3,
+  post_id:    13,
+  title:      "my favorite",
+  comment:    "Please try to go to Niigata next time.",
+  rate:       4.5,
+)
--- db/seeds/user.rb
+++ db/seeds/user.rb
@@ -8,13 +8,73 @@ User.create!(
   password:        "adminadmin"
 )
 
-5.times do |n|
-  User.create!(
-    name:            "test#{n + 1}",
-    email:           "test#{n + 1}@test",
-    age:             n + 1,
-    country_code:    "US",
-    withdrawal_flag: "false",
-    password:        "111111"
-  )
-end
+# 5.times do |n|
+#   User.create!(
+#     name:            "test#{n + 1}",
+#     email:           "test#{n + 1}@test",
+#     age:             n + 1,
+#     country_code:    "US",
+#     withdrawal_flag: "false",
+#     password:        "111111"
+#   )
+# end
+
+# 2
+User.create!(
+  name:            "田中太郎",
+  email:           "tanaka@email.com",
+  age:             "24",
+  country_code:    "JP",
+  withdrawal_flag: "false",
+  password:        "tanakadesu"
+)
+
+# 3
+User.create!(
+  name:            "山田次郎",
+  email:           "jirojiro@email.com",
+  age:             "41",
+  country_code:    "JP",
+  withdrawal_flag: "false",
+  password:        "yamadajiro21"
+)
+
+# 4
+User.create!(
+  name:            "John Smith",
+  email:           "john1222@ezweb.com",
+  age:             "31",
+  country_code:    "US",
+  withdrawal_flag: "false",
+  password:        "john1222"
+)
+
+# 5
+User.create!(
+  name:            "黄秀英",
+  email:           "fanfan@gmail.com",
+  age:             "18",
+  country_code:    "CN",
+  withdrawal_flag: "false",
+  password:        "chichifan"
+)
+
+# 6
+User.create!(
+  name:            "Andrea Muller",
+  email:           "eeeeand@gmail.com",
+  age:             "42",
+  country_code:    "DE",
+  withdrawal_flag: "false",
+  password:        "germanyandrea"
+)
+
+# 7
+User.create!(
+  name:            "金 本貫",
+  email:           "eeeeand@gmail.com",
+  age:             "42",
+  country_code:    "KR",
+  withdrawal_flag: "false",
+  password:        "kimkimkim"
+)
